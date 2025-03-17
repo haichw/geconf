2307f967c16aa42c3bfad017203e2c32e1b68b3e
Walysson Coelho <walysson2020@gmail.com>
2025-03-16 21:57:54 -0300

    Hello World

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, world!"
+    return "Hello, world no navegador e docker!"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
